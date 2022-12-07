955c25d774233e164340296d8d49fd7fdb669332
kmakai <makaikwag@gmail.com>
2022-12-06 21:11:34 -0500

    looking for ways to dynamically get areas on image with target boxes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import conimage from "./assets/conimage.png";
 
 function App() {
+  const onClick = (e) => {
+    console.log(e.pageX, e.pageY);
+
+    const startX = 345;
+    const startY = 240;
+    const endX = 395;
+    const endY = 320;
+    if (
+      e.pageX > startX &&
+      e.pageX < endX &&
+      e.pageY > startY &&
+      e.pageY < endY
+    ) {
+      console.log("inside");
+    } else {
+      console.log("outside");
+    }
+  };
+
+  function rev(e) {
+    console.log(e.target.id);
+  }
+
+  useEffect(() => {
+    function handleResize() {
+      console.log(window.innerHeight, window.innerWidth);
+      console.log("some");
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={onClick}>
+      <div className="targetbox1" id="naruto" onClick={rev}></div>
+      <div className="targetbox2" id="zoid" onClick={rev}></div>
+      <div className="targetbox3" id="aang" onClick={rev}></div>
+      <img src={`${conimage}`} alt="con" />
     </div>
   );
 }
